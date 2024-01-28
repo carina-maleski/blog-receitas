6e6c882fad9df26c90638e24fdde64c7c906d75f
carina-maleski <carinamaleski@gmail.com>
2024-01-28 15:48:09 -0300

    created post and put methods at CategoriaController

--- blogreceitas/src/main/java/com/generation/blogreceitas/controller/CategoriaController.java
+++ blogreceitas/src/main/java/com/generation/blogreceitas/controller/CategoriaController.java
@@ -8,11 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.blogreceitas.model.Categoria;
 import com.generation.blogreceitas.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/categorias")
@@ -36,6 +41,18 @@ public class CategoriaController {
 	@GetMapping("/titulo/{categoriaTitulo}")
 	public ResponseEntity<List<Categoria>> findByCategoriaTitulo(@PathVariable String categoriaTitulo) {
 		return ResponseEntity.ok(categoriaRepository.findAllByTituloCategoriaContainingIgnoreCase(categoriaTitulo));
+	}
+
+	@PostMapping
+	public ResponseEntity<Categoria> criarNovaCategoria(@Valid @RequestBody Categoria categoria) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria));
+	}
+
+	@PutMapping
+	public ResponseEntity<Categoria> atualizarCategoria(@Valid @RequestBody Categoria categoria) {
+		return categoriaRepository.findById(categoria.getId())
+				.map(resp -> ResponseEntity.status(HttpStatus.CREATED).body(categoriaRepository.save(categoria)))
+				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}
 
 }
